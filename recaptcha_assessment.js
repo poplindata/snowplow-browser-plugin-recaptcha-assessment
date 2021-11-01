6d73a5ba757b40dfd6c0b0c0a0d59598e5242f32
Jethro Nederhof <jethro@jethron.id.au>
2021-11-02 10:42:18 +1100

    Preliminary hCaptcha support

--- recaptcha_assessment.js
+++ recaptcha_assessment.js
@@ -15,33 +15,58 @@ Object.defineProperty(
             f = "function",
             d = document;
 
-          var state = {
-            siteKey: opts.siteKey,
-            action: opts.action || "page_view",
-            sendEvent: typeof opts.sendEvent === u ? false : opts.sendEvent,
-            sendContext:
-              typeof opts.sendContext === u ? true : opts.sendContext,
-            schema:
-              opts.schema ||
-              "iglu:com.google.recaptcha/action/jsonschema/1-0-0",
-            altDomain:
-              opts.altDomain === true
-                ? "www.recaptcha.net"
-                : opts.altDomain || "www.google.com",
-            callback:
-              typeof opts.callback === "function" ? opts.callback : undefined,
-            enterprise: !!opts.enterprise,
-
-            token: undefined,
-            challenge_ts: undefined,
-            loaded: false,
-            pending: 0,
-          };
+          var state = Object.assign(
+            {
+              token: undefined,
+              challenge_ts: undefined,
+              loaded: false,
+              pending: 0,
+            },
+            {
+              siteKey: opts.siteKey,
+              sendEvent: typeof opts.sendEvent === u ? false : opts.sendEvent,
+              sendContext:
+                typeof opts.sendContext === u ? true : opts.sendContext,
+              callback:
+                typeof opts.callback === "function" ? opts.callback : undefined,
+              params: state.params || {},
+            },
+            opts.hCaptcha
+              ? {
+                  hCaptcha: true,
+                  schema:
+                    opts.schema ||
+                    "iglu:com.hcaptcha/challenge/jsonschema/1-0-0",
+                  altDomain:
+                    typeof opts.altDomain === "string"
+                      ? opts.altDomain
+                      : "js.hcaptcha.com",
+                  pathPrefix: opts.pathPrefix || "/1/",
+                  pathFileName: opts.pathFileName || "api.js",
+                  enterprise: false,
+                }
+              : {
+                  hCaptcha: false,
+                  action: opts.action || "page_view",
+                  schema:
+                    opts.schema ||
+                    "iglu:com.google.recaptcha/action/jsonschema/1-0-0",
+                  altDomain:
+                    opts.altDomain === true
+                      ? "www.recaptcha.net"
+                      : opts.altDomain || "www.google.com",
+                  pathPrefix: opts.pathPrefix || "/recaptcha/",
+                  pathFileName: opts.pathFileName || "api.js",
+                  enterprise: !!opts.enterprise,
+                }
+          );
 
           if (typeof state.siteKey !== "string" || !state.siteKey) {
             console.warn("Snowplow/ReCAPTCHA: missing siteKey");
             return {};
           }
+
+          state.params.render = state.params.render || state.siteKey;
 
           function payload() {
             return {
@@ -49,7 +74,7 @@ Object.defineProperty(
               data: {
                 token: state.token,
                 site_key: state.siteKey,
-                action: state.action,
+                action: !state.hCaptcha ? state.action : undefined,
                 challenge_ts: state.challenge_ts,
               },
             };
@@ -68,14 +93,25 @@ Object.defineProperty(
                 state.loaded = true;
                 var scripts = d.getElementsByTagName(s);
                 var api = d.createElement(s);
-                api.src = [
+                var src = [
                   "//",
                   state.altDomain,
-                  "/recaptcha/",
-                  state.enterprise ? "enterprise.js" : "api.js",
-                  "?render=",
-                  state.siteKey,
+                  state.pathPrefix,
+                  state.enterprise ? "enterprise.js" : state.pathFileName,
+                  "?",
                 ].join("");
+
+                for (var p in state.params) {
+                  if (state.params.hasOwnProperty(p)) {
+                    src =
+                      src +
+                      ["&", p, "=", encodeURIComponent(state.params[p])].join(
+                        ""
+                      );
+                  }
+                }
+
+                api.src = src;
                 api.async = 1;
                 api.addEventListener(
                   "load",
@@ -84,25 +120,30 @@ Object.defineProperty(
                     var gr = grecaptcha.enterprise || grecaptcha;
                     gr.ready(function () {
                       state.challenge_ts = new Date();
-                      gr.execute(state.siteKey, { action: state.action }).then(
-                        function (token) {
-                          state.token = token;
-                          var trackerfn = tracker.id.replace(
-                            "_" + tracker.namespace,
-                            ""
-                          );
-                          if (
-                            state.sendEvent &&
-                            typeof window[trackerfn] === f
-                          ) {
-                            window[trackerfn]("trackSelfDescribingEvent", {
-                              event: payload(),
-                            });
-                          }
+                      gr.execute(
+                        !state.hCaptcha ? state.siteKey : undefined,
+                        !state.hCaptcha
+                          ? { action: state.action }
+                          : { async: true }
+                      ).then(function (token) {
+                        state.token = token;
 
-                          if (state.callback) callback(token);
+                        // at this point, tracker doesn't appear to include a trackSelfDescribingEvent method
+                        // so we need to call the tracker globally
+
+                        // find the tracking function name
+                        var trackerfn = tracker.id.replace(
+                          "_" + tracker.namespace,
+                          ""
+                        );
+                        if (state.sendEvent && typeof window[trackerfn] === f) {
+                          window[trackerfn]("trackSelfDescribingEvent", {
+                            event: payload(),
+                          });
                         }
-                      );
+
+                        if (state.callback) callback(token);
+                      });
                     });
                   },
                   false
